358b1e24fb0dea7c830429968c6294d84bdb266f
Tenrys <tenrys.iaido@live.fr>
2017-02-03 19:17:05 +0100

    Translate rules better

--- french/website/rules.js
+++ french/website/rules.js
@@ -5,36 +5,36 @@ module.exports = {
 			[
 				"Section Principale",
 				[
-					"Il est interdit d'utiliser des logiciels ou des bugs, qui te donne un avantage sur les autres joueurs (Cheats,BugUse).",
+					"Il est interdit d'utiliser des logiciels ou des bugs, qui peuvent procurer un avantage sur les autres joueurs (Cheats, UseBug).",
 					"Il est interdit de perturber le fonctionnement du serveur.",
-					"Il est interdit de d'outrepasser le ban d'un compte.",
-					`Les joueurs doivent informer l'administration sur un problème important, qui peut donner la capacité au serveur de crash ou de donner un désavantage sur les autres joueurs.
+					"Il est interdit d'outrepasser le ban d'un compte.",
+					`Les joueurs doivent informer l'administration des problèmes importants, qui peuvent faire crasher le serveur ou désavantager les autres joueurs.
 					<br>Il est punissable de posséder et de cacher de telles informations, ainsi que de la transmettre à d'autres joueurs.`,
-					"Ton nom ne doit pas avoir des mots avec différentes lettres de différentes langues."
+					"Votre nom ne doit pas avoir des mots avec différentes lettres de différentes langues."
 				]
 			],
 			[
 				"Règles du chat",
 				[
-					"Il n'est pas recommandé d'insulter d'autres joueurs.",
-					"Diverses affirmations ressenties et insensées (\"ce serveur est nul\", \"tout est clair avec vous\", \"quel admin est ici...\" et autres) va seulement vous mener vers un ban.",
-					"Les discriminations basées sur le sexe d'une personne sont interdites..",
+					"Vous ne devriez pas insulter les autres joueurs.",
+					"Toute forme d'affirmation ressentie et insensée (\"ce serveur est nul\", et autres) vous menera uniquement vers un ban.",
+					"Les discriminations basées sur le sexe d'une personne sont interdites.",
 					"Le harcèlement sexuel est interdit.",
-					"Il est interdit de faire de la publicité pour des serveurs, des communautés, des groupes steam sous toutes les formes.",
-					"Il est interdit de répéter le même message dans un court laps de temps.",
+					"Il est interdit de faire de la publicité pour des serveurs, des communautés, des groupes Steam sous toutes les formes.",
+					"Il est interdit de répéter le même message dans un court laps de temps. (Spam)",
 					"Il est interdit d'envoyer un message en Publique (inter-serveur) chat, qui sont inutiles en dehors de ce serveur.",
-					"Tu devras utiliser le chat global pour contacter un administrateur uniquement dans les cas critiques (quelqu'un essaie de faire crash le serveur ou utilise un cheat).",
-					"N'utilise pas le chat vocal pour faire des sons forts, des grincements et d'autres choses déplaisantes à écouter."
+					"Vous devrez utiliser le chat global pour contacter un administrateur uniquement dans les cas critiques (quelqu'un essaie de faire crash le serveur ou utilise un cheat).",
+					"N'utilisez pas le chat vocal pour faire des sons bruyants, des grincements et d'autres choses déplaisantes à écouter."
 				]
 			],
 			[
 				"Communication avec l'administration",
 				[
-					"Soyez polis et patient tout en parlant avec un administrateur.",
-					"Quand tu appelles un administrateur, tu dois indiquer le problème et le nom du joueur offensant.",
-					"Ne dites pas aux administrateurs qu'ils sont dégradés, vous n'avez aucun pouvoir.",
-					"N'ajoutez pas les admins sur Steam ou d'autres moyens pour communiquer sur internet. Utilisez le forum.",
-					"Les administrateurs aident exclusivement à leur propre gré. Ils ne sont pas obligés de répondre."
+					"Soyez polis et patient quand vous communiquez avec un administrateur.",
+					"Quand vous appellez un administrateur, vous devez indiquer le problème et le nom du joueur offensant.",
+					"Ne menacez pas les administrateurs de les faire rétrograder, vous n'avez aucun pouvoir.",
+					"N'ajoutez pas les admins sur Steam ou d'autres moyens pour communiquer sur Internet. Utilisez le forum.",
+					"Les administrateurs ne sont pas à votre disposition. Ils ne sont pas obligés de répondre."
 				]
 			]
 		]
@@ -46,8 +46,8 @@ module.exports = {
 			[
 				"Section principale",
 				[
-					`Votre nom roleplay doit être réaliste (et non Cat Meow, L'Homme Crabe), il ne devra pas contenir de ponctuation et devra suivre les règles générales de capitalisation des mots.`,
-					`Il est interdit d'utiliser le suicide, se reconnecter, changer de métier pour s'évader d'une situation RP, tel que les poursuites par la police, les braquages, devenir un otage, la faim ou autre.`,
+					`Votre nom roleplay doit être réaliste (et non Chat Miaou, L'Homme Crabe), il ne devra pas contenir de ponctuation et devra suivre les règles générales de capitalisation des mots.`,
+					`Il est interdit d'utiliser le suicide, de se reconnecter, ou de changer de métier pour échapper à situation RP, tel que les poursuites par la police, les braquages, devenir un otage, la faim ou autre.`,
 					`Votre mission n'est pas de vaincre tout le monde, ne tuez pas toutes les personnes que vous voyez sur votre chemin. Tout meurtre devra suivre avec une raison adéquate. (Random DeathMatch, RDM)`,
 					`Tout métier peut avoir une arme pour se défendre, sauf celles interdites par les lois.`,
 					`Les chefs de gangs sont "invisibles" - votre personnage ressemble à un citoyen normal (ce qui signifie, vous ne pouvez pas juste fouiller un chef de gang pour des armes, juste parce qu'il est chef).<br>
@@ -60,23 +60,23 @@ module.exports = {
 							<li>Sauter dans le feu avec courage, là où la sortie est inaccessible.
 							<li>Rester devant une voiture en mouvement.
 						</ul>`,
-					`Tout vote de renvoie (Premium seulement) doit avoir une appropriée et véridique raison en accord avec les règles, sans insultes. Vous n'êtes pas autorisés à initier des votes de t'envoies s'il n'y a aucunes règles qui ne sont pas respectées.`,
-					`Votez sagement, ne votez pas aléatoirement en appuyant sur "F5" et "F6" dans les votes. Si vous n'êtes pas sûr - appuyez sur "ne pas voter".`,
-					`Il est strictement interdit d'échange de l'argent dans le jeu contre de l'argent réel ou ses équivalents.`,
+					`Tout vote de renvoi (Premium seulement) doit avoir une raison appropriée et en accord avec les règles, sans insultes. Vous n'êtes pas autorisés à initier des votes de renvoi s'il n'y a aucune règle qui n'est pas respectée.`,
+					`Votez sagement, ne votez pas aléatoirement en appuyant sur "F5" et "F6" dans les votes. Si vous n'êtes pas sûr de vous - appuyez sur "ne pas voter".`,
+					`Il est strictement interdit d'échanger de l'argent dans le jeu contre de l'argent réel ou ses équivalents.`,
 				]
 			],
 			[
 				"Règles du chat",
 				[
-					`N'envoyez pas d'informations roleplay dans le chat OOC (MetaGaming).<br>
-						Vous n'êtes pas autorisé à utiliser des informations obtenues dans le chat OOC (Out of Character).`,
-					`N'utilisez pas le OOC pour communiquer avec une personne en particulier.<br>
+					`N'envoyez pas d'informations roleplay dans le chat HRP (MetaGaming).<br>
+						Vous n'êtes pas autorisé à utiliser des informations obtenues dans le chat HRP (Hors Role Play).`,
+					`N'utilisez pas le HRP pour communiquer avec une personne en particulier.<br>
 						Pour discuter avec quelqu'un en privé utilisez le PM tab dans le chat.`,
 					`Il est interdit de commettre des actions RolePlay sexuellement explicites.`,
 					`Il est interdit d'envoyer un message en /advert, qui n'est pas une publicité RP.`,
-					`N'utilisez pas le chat vocal si vous avez moins de 14ans.`,
-					`Le téléphone est une partie du RP, ne l'utilisez pas pour contacter un administrateur pour avoir de l'aide.`,
-					`Ne menacez pas les autres joueurs avec les bans. Vous n'êtes pas un modérateur.`,
+					`Évitez d'utiliser le chat vocal si votre voix est insupportable et qu'on vous le fait remarquer.`,
+					`Le téléphone est une partie du RP, ne l'utilisez pas pour contacter un administrateur afin d'obtenir de l'aide.`,
+					`Ne menacez pas les autres joueurs de les bannir. Vous n'êtes pas un modérateur.`,
 				]
 			],
 			[
@@ -91,7 +91,7 @@ module.exports = {
 						Vous POUVEZ fouiller les maisons sans raison.`,
 					`Les policiers corrompus ne peuvent pas contrecarrer activement l'application des lois. Par exemple, les policiers corrompus peuvent "ne pas notifier" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais il ne peut pas attaquer un autre policer.`,
 					`Le maire n'est pas autorisé à faire des lois qui ne respectent pas les règles du serveur.`,
-					`La torture des prisonniers est régi par le maire ou le chef de la police.`,
+					`La torture des prisonniers est régie par le maire ou le chef de la police.`,
 				]
 			],
 			[
@@ -104,10 +104,10 @@ module.exports = {
 			[
 				"Règles des commerçants & des médecins",
 				[
-					`N'importe quel homme d'affaire ne peut pas être au service d'un joueur, qui leur inflige des dégâts, les insultes, perturber la paix ou les recherchés par la police.<br>
-						S'il n'y a pas de raison suffisante pour décliner un échange, l'homme devrait le commettre.`,
+					`Si un de vos clients vous agresse ou vous insulte, perturbe la paix dans votre échoppe ou est recherché par la police, vous pouvez vous réserver le droit de ne pas servir celui-ci.<br/>
+						Dans le cas contraire où le client n'a rien fait de mal, vous vous devez de le servir.`,
 					`Les vendeurs d'armes, les médecins, les commerçants ne sont pas autorisés à utiliser leurs capacités à des fins criminelles ou de participer à des activités criminelles.`,
-					`Tout les hommes d'affaires devraient faire des profits sur leurs ventes.`,
+					`Tous les hommes d'affaires devraient faire des profits sur leurs ventes.`,
 				]
 			],
 			[
@@ -128,27 +128,27 @@ module.exports = {
 					`Il est interdit de pousser des objets ou des joueurs en utilisant des props (PropPush).`,
 					`Vos constructions doivent être réalistes: elles doivent être plus bas que la skybox, avoir un support (comme, un vrai support, et non un cylindre avec un ascenseur), et ne devra pas changer de couleur rapidement.`,
 					`Il est interdit de bloquer une entrée publique.<br>
-						Si l'entrée suivi d'un couloir, où les portes vous appartiennent, vous êtes autorisé à la bloquer.`,
-					`Toute construction devra posséder une entrée, qui est publiquement accessible, ou crackable avec un pied de biche, un keypad ou un bouton.`,
-					`Toute entrée qui est faite par un mappeur devra être publiquement accessible, ou crackable avec un pied de biche, un keypad ou un bouton.`,
-					`Les objets, cachés derrière/dessous/dessus un props, devra être accessible grâce à un pied de biche, un Keypad ou un bouton.`,
+						Si l'entrée est suivie d'un couloir, et les portes vous appartiennent, vous êtes autorisé à la bloquer.`,
+					`Toute construction devra posséder une entrée, publiquement accessible avec un keypad ou un bouton et être crochettable.`,
+					`Toute entrée qui est faite par un mappeur devra être publiquement accessible avec un keypad ou un bouton et être crochettable.`,
+					`Les objets, cachés derrière/dessous/dessus un props, devra être accessible avec un keypad ou un bouton et être crochettable.`,
 					`Vous n'êtes pas autorisé à utiliser plus de deux couches de portes (les portes usuels sont aussi prise en compte).<br>
 						Si cette porte est de plus de deux props, il devra y avoir un keypad facilement visible près de la porte, qui ouvre toute l'entrée au même moment.`,
-					`Les boutons/keypads doivent être proches de l'objet auquel ils contrôlent, ils devront être perceptibles.`,
-					`Les entrées devront être capable de tenir deux joueur et être en taille entière, se qui signifie qu'il doit faire la taille d'un joueur qui se tient debout (64 unités), donc les tunnels qui demandent à être accroupis, ou les couloirs qui demandent des esquives/des sauts/des deviations ne sont pas autorisées.`,
+					`Les boutons/keypads doivent être proches de l'objet auquel ils contrôlent et facilement visibles.`,
+					`Les entrées devront être capable de contenir deux joueurs et être en taille entière, ce qui signifie qu'il doit faire la taille d'un joueur qui se tient debout (64 unités), donc les tunnels qui demandent à être accroupis, ou les couloirs qui demandent des esquives/des sauts/des deviations ne sont pas autorisées.`,
 					`Il est interdit d'utiliser, créer et supprimer des fading doors pendant une bataille.`,
 					`Les chambres de tirs doivent mesurer moins de la moitié d'une plaque standard (metal_plate1, 1x1 PHX) en hauteur et en largeur.`,
 					`Les portes, les caméras, les ponts, les boutons et d'autres objets doivent être clairement visibles.`,
-					`Il est interdit d'utiliser un props qui ressemblent à un mur, mais qui agissent comme un mur sans collisions et tirer à travers et vice versa.`,
-					`Il est interdit d'utiliser un props qui est transparent que d'un seul côté.`,
+					`Il est interdit d'utiliser un prop qui ressemble à un mur, mais qui agit comme un mur sans collisions et tirer à travers et vice versa.`,
+					`Il est interdit d'utiliser un prop qui n'est transparent que d'un seul côté.`,
 					`Il est interdit de placer des structures gênantes sur des territoires qui ne vous appartiennent pas sans le consentement du propriétaire.`,
-					`Vous devriez déclarer un territoire neutre comme le votre et construire une maison dessus, si vous n'êtes pas dans un territoire public (route, parking).`,
-					`Il est interdit de faire apparaître des objets précieux et des bâtiments dans des endroits, qui ne sont accessibles par les moyens de transports usuels (sans parcours).<br>
-						xception: le toit d'un maison en dessous de quatre magasins. Vous devez construire une échelle accessible publiquement ou un ascenseur jusqu'à ce tout. Quand il n'y a pas de moyen pour y accéder, vous n'êtes pas autorisé à placer des objets précieux et des bâtiments sur ce toit.`,
+					`Vous pouvez déclarer un territoire neutre comme le votre et construire une maison dessus, si vous n'êtes pas dans un territoire public (route, parking).`,
+					`Il est interdit de faire apparaître des objets précieux et des bases dans des endroits, qui ne sont pas accessibles par les moyens de transports usuels (sans Parkour).<br>
+						Exception: le toit d'un maison en dessous de quatre magasins. Vous devez construire une échelle accessible publiquement ou un ascenseur jusqu'à ce tout. Quand il n'y a pas de moyen pour y accéder, vous n'êtes pas autorisé à placer des objets précieux et des bases sur ce toit.`,
 					`Si un ascenseur est requis pour accéder à votre structure, puis contrôler cet ascenseur devra être disponible sans crackage. Il est interdit de limiter l'accès de cet ascenseur. L'ascenseur devra marcher de manière stable. Ce qui veut dire:
 					<ul>
 						<li>Votre ascenseur devra être construit avec l'outil spécial DarkRP -> Elevator
-						<li> Il ne devra y avoir aucune porte ou mur a l'entrée ou à la sortie.
+						<li>Il ne devra y avoir aucune porte ou mur à l'entrée ou à la sortie.
 					</ul>`,
 					`Il est interdit de faire disparaître des sols, des échelles, des ascenseurs et des plateformes`,
 					`Il est interdit de faire apparaître des prisons pour les joueurs/objets en utilisant le duplicateur sur des joueurs/objets, ou mettre des props sur des joueurs/objets avec le physgun.`,
