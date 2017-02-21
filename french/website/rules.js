29869f90d82e33dac96df50e76c69051913acca9
Erwan Oliviero <oliviero@outlook.fr>
2017-02-21 19:00:41 +0100

    Some modifications.
    Some modifications, it is not urgent to apply it right now. The modifications are just corrections.

--- french/website/rules.js
+++ french/website/rules.js
@@ -9,7 +9,7 @@ module.exports = {
 					"Il est interdit de perturber le fonctionnement du serveur.",
 					"Il est interdit d'outrepasser le ban d'un compte.",
 					`Les joueurs doivent informer l'administration des problèmes importants, qui peuvent faire crasher le serveur ou désavantager les autres joueurs.
-					<br>Il est punissable de posséder et de cacher de telles informations, ainsi que de la transmettre à d'autres joueurs.`,
+					<br>Il est punissable de posséder et de cacher de telles informations, ainsi que de les transmettre à d'autres joueurs.`,
 					"Votre nom ne doit pas avoir des mots avec différentes lettres de différentes langues."
 				]
 			],
@@ -22,7 +22,7 @@ module.exports = {
 					"Le harcèlement sexuel est interdit.",
 					"Il est interdit de faire de la publicité pour des serveurs, des communautés, des groupes Steam sous toutes les formes.",
 					"Il est interdit de répéter le même message dans un court laps de temps. (Spam)",
-					"Il est interdit d'envoyer un message en Publique (inter-serveur) chat, qui sont inutiles en dehors de ce serveur.",
+					"Il est interdit d'envoyer un message en Publique (inter-serveur) chat, qui est inutile en dehors de ce serveur.",
 					"Vous devrez utiliser le chat global pour contacter un administrateur uniquement dans les cas critiques (quelqu'un essaie de faire crash le serveur ou utilise un cheat).",
 					"N'utilisez pas le chat vocal pour faire des sons bruyants, des grincements et d'autres choses déplaisantes à écouter."
 				]
@@ -50,13 +50,13 @@ module.exports = {
 					`Il est interdit d'utiliser le suicide, de se reconnecter, ou de changer de métier pour échapper à situation RP, tel que les poursuites par la police, les braquages, devenir un otage, la faim ou autre.`,
 					`Votre mission n'est pas de vaincre tout le monde, ne tuez pas toutes les personnes que vous voyez sur votre chemin. Tout meurtre devra suivre avec une raison adéquate. (Random DeathMatch, RDM)`,
 					`Tout métier peut avoir une arme pour se défendre, sauf celles interdites par les lois.`,
-					`Les chefs de gangs sont "invisibles" - votre personnage ressemble à un citoyen normal (ce qui signifie, vous ne pouvez pas juste fouiller un chef de gang pour des armes, juste parce qu'il est chef).<br>
+					`Les chefs de gangs sont "invisibles" - votre personnage ressemble à un citoyen normal (ce qui signifie, vous ne pouvez pas fouiller un chef de gang, juste parce qu'il est chef).<br>
 						Toutes les autres informations affichées sur l'écran sont aussi vues par votre personnage.`,
 					`Ne changez pas de métier dans un court laps de temps pour avoir des objets, des armes ou d'autres choses (abuser des métiers).`,
 					`Ne faites pas quelque chose de stupide qu'un personne normale ne devrait pas faire, dire:
 						<ul>
 							<li>Faire apparaître 1 000 000 affiches qui disent "zone de kill".
-							<li>Demander a un policier de vous tuer juste car vous avez faim.
+							<li>Demander à un policier de vous tuer juste car vous avez faim.
 							<li>Sauter dans le feu avec courage, là où la sortie est inaccessible.
 							<li>Rester devant une voiture en mouvement.
 						</ul>`,
@@ -72,7 +72,7 @@ module.exports = {
 						Vous n'êtes pas autorisé à utiliser des informations obtenues dans le chat HRP (Hors Role Play).`,
 					`N'utilisez pas le HRP pour communiquer avec une personne en particulier.<br>
 						Pour discuter avec quelqu'un en privé utilisez le PM tab dans le chat.`,
-					`Il est interdit de commettre des actions RolePlay sexuellement explicites.`,
+					`Il est interdit de commettre des actions roleplay sexuellement explicites.`,
 					`Il est interdit d'envoyer un message en /advert, qui n'est pas une publicité RP.`,
 					`Évitez d'utiliser le chat vocal si votre voix est insupportable et qu'on vous le fait remarquer.`,
 					`Le téléphone est une partie du RP, ne l'utilisez pas pour contacter un administrateur afin d'obtenir de l'aide.`,
@@ -84,10 +84,10 @@ module.exports = {
 				[
 					`Les officiers de police ne sont pas autorisés à arrêter quelqu'un sans raison adéquate (Random Arrest).`,
 					`Utilisez le taser et les menottes uniquement contre les perturbateurs.`,
-					`Tout les mandats et les couvres-feu doivent être suivies par une raison cohérente. Les raisons tel que "lol", "fouille", "ccccccc" sont invalides.`,
+					`Tous les mandats et les couvres-feu doivent être suivis par une raison cohérente. Les raisons tel que "lol", "fouille", "ccccccc" sont invalides.`,
 					//`Арест должен производиться только через кнопку "арестовать" в наручниках (а не ведением нарушителя в наручниках пешком до ПУ и удержания там).`,
-					`La police est autorisée à construire des barrières, des points de contrôle, des dos-d'ânes, bloquer des rues et des bâtiments. Toutes les barrières qui bloquent quelque chose doivent avoir un entrée crackable.`,
-					`Les fouilles pour trouver des armes n'exige pas de mandat (mais une raison adéquate est requise).<br>
+					`La police est autorisée à construire des barrières, des points de contrôle, des dos-d'ânes et à bloquer des rues ou des bâtiments. Toutes les barrières qui bloquent quelque chose doivent avoir un entrée crackable.`,
+					`Les fouilles pour trouver des armes n'exigent pas de mandat (mais une raison adéquate est requise).<br>
 						Vous POUVEZ fouiller les maisons sans raison.`,
 					`Les policiers corrompus ne peuvent pas contrecarrer activement l'application des lois. Par exemple, les policiers corrompus peuvent "ne pas notifier" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais il ne peut pas attaquer un autre policer.`,
 					`Le maire n'est pas autorisé à faire des lois qui ne respectent pas les règles du serveur.`,
@@ -111,11 +111,11 @@ module.exports = {
 				]
 			],
 			[
-				"Règles des tueurs à gage",
+				"Règles des mercenaires",
 				[
-					`Les demandes d'exécution doivent uniquement être créés à partir du système de tueur à gage (appuyez sur E sur un tueur à gage ou un chef de gang). Les demandes d'exécution faites sous d'autres formes ne sont pas valides.`,
+					`Les demandes d'exécution doivent uniquement être créées à partir du système du mercenaire (appuyez sur E sur un mercenaire ou un chef de gang). Les demandes d'exécution faites sous d'autres formes ne sont pas valides.`,
 					`Toutes les demandes d'exécution devront suivre avec une raison adéquate. Exécuter sans raison est du RandomDeathMatch (RDM).`,
-					`Le client est responsable en cas d'exécution aléatoire, pas en mercenaire.`,
+					`Le client est responsable en cas d'exécution aléatoire, pas le mercenaire.`,
 				]
 			],
 			[
@@ -132,10 +132,10 @@ module.exports = {
 					`Toute construction devra posséder une entrée, publiquement accessible avec un keypad ou un bouton et être crochettable.`,
 					`Toute entrée qui est faite par un mappeur devra être publiquement accessible avec un keypad ou un bouton et être crochettable.`,
 					`Les objets, cachés derrière/dessous/dessus un prop, devront être accessibles avec un keypad ou un bouton et être crochettable.`,
-					`Vous n'êtes pas autorisé à utiliser plus de deux couches de portes (les portes usuels sont aussi prise en compte).<br>
+					`Vous n'êtes pas autorisé à utiliser plus de deux couches de portes (les portes usuelles sont aussi prise en compte).<br>
 						Si cette porte est de plus de deux props, il devra y avoir un keypad facilement visible près de la porte, qui ouvre toute l'entrée au même moment.`,
-					`Les boutons/keypads doivent être proches de l'objet auquel ils contrôlent et facilement visibles.`,
-					`Les entrées devront être capable de contenir deux joueurs et être en taille entière, ce qui signifie qu'il doit faire la taille d'un joueur qui se tient debout (64 unités), donc les tunnels qui demandent à être accroupis, ou les couloirs qui demandent des esquives/des sauts/des deviations ne sont pas autorisées.`,
+					`Les boutons/keypads doivent être proches de l'objet qu'ils contrôlent et facilement visibles.`,
+					`Les entrées devront être capable de contenir deux joueurs et être en taille entière, ce qui signifie qu'il doit faire la taille d'un joueur qui se tient debout (64 unités), donc les tunnels qui demandent à être accroupis, ou les couloirs qui demandent des esquives/des sauts/des deviations ne sont pas autorisés.`,
 					`Il est interdit d'utiliser, créer et supprimer des fading doors pendant une bataille.`,
 					`Les chambres de tirs ne doivent pas mesurer moins de la moitié d'une plaque standard (metal_plate1, 1x1 PHX) en hauteur et en largeur.`,
 					`Les portes, les caméras, les ponts, les boutons et d'autres objets doivent être clairement visibles.`,
@@ -150,7 +150,7 @@ module.exports = {
 						<li>Votre ascenseur devra être construit avec l'outil spécial DarkRP -> Elevator
 						<li>Il ne devra y avoir aucune porte ou mur à l'entrée ou à la sortie.
 					</ul>`,
-					`Il est interdit de faire disparaître des sols, des échelles, des ascenseurs et des plateformes`,
+					`Il est interdit de faire des sols, des échelles, des ascenseurs et des plateformes disparaissant`,
 					`Il est interdit de faire apparaître des prisons pour les joueurs/objets en utilisant le duplicateur sur des joueurs/objets, ou mettre des props sur des joueurs/objets avec le physgun.`,
 					`Il est interdit d'utiliser le wiremod pour rechercher des joueurs ou des objets précieux à l'extérieur de votre territoire.`,
 					`Vous n'êtes pas autorisé à construire durant un combat ou d'autres formes de confrontation.`,
