2424d8104f842459d237c58586e521c5bd6a7771
Erwan Oliviero <oliviero@outlook.fr>
2017-07-10 18:33:25 +0200

    Rules are now CLEAR for players and moders
    Rules are now CLEAR for players and moders (curfews, warrants, etc.)

--- french/website/rules.js
+++ french/website/rules.js
@@ -54,7 +54,7 @@ module.exports = {
 					`Les chefs de gangs sont "invisibles" - ils ressemblent à des citoyens normaux (ce qui signifie que vous ne pouvez pas fouiller un chef de gang, juste parce que vous voyez qu'il est chef).<br>
 						Toutes les autres informations affichées sur l'écran sont aussi vues par votre personnage.`,
 					`Ne changez pas de métier dans l'unique but d'aquérir des objets, des armes, des capacités ou n'importe quoi d'autre (JobAbuse).`,
-					`Ne faites pas quelque chose de stupide qu'une personne normale ne ferait pas, c'est-à-dire:
+					`Ne faites pas quelque chose d'impossible qu'une personne réelle ne ferait pas, c'est-à-dire:
 						<ul>
 							<li>Faire apparaître 1 000 000 affiches qui disent "zone de kill".
 							<li>Demander à un policier de vous tuer juste car vous avez faim.
@@ -74,21 +74,21 @@ module.exports = {
 					`Il est interdit de commettre des actions rôleplay sexuellement explicites.`,
 					`Il est interdit d'envoyer un message en /advert, qui n'est pas une publicité RP.`,
 					`Évitez d'utiliser le chat vocal si votre voix est désagréable et qu'on vous le fait remarquer.`,
-					`Le téléphone est une partie du RP, ne l'utilisez pas pour contacter un administrateur afin d'obtenir de l'aide.`,
-					`Ne menacez pas les autres joueurs de les bannir. Vous n'êtes pas un modérateur.`,
+					`N'utilisez pas le portable ainsi que le /advert pour appeler un admin.`,
+					`Ne menacez pas les autres joueurs de les bannir.`,
 				]
 			],
 			[
-				"Règles d'application de la loi",
+				"Règles des forces de l'ordre",
 				[
 					`Les officiers de police ne sont pas autorisés à arrêter quelqu'un sans raison adéquate (Random Arrest).`,
 					`Utilisez le taser et les menottes uniquement contre les perturbateurs.`,
-					`Tous les mandats et les couvres-feu doivent être suivis par une raison cohérente. Les raisons telles que "lol", "fouille", "ccccccc" sont invalides.`,
+					`Toutes les recherches, les couvre-feux et les fouilles d'armes (détecteur de métaux), doivent être suivis par une raison cohérente. Les raisons telles que "lol", "fouille", "ccccccc" sont invalides.`,
 					//`Арест должен производиться только через кнопку "арестовать" в наручниках (а не ведением нарушителя в наручниках пешком до ПУ и удержания там).`,
 					`La police est autorisée à construire des barrières, des points de contrôle, des dos-d'ânes ainsi qu'à bloquer des rues ou des bâtiments. Toutes les barrières qui bloquent quelque chose doivent avoir un entrée crackable.`,
-					`Les fouilles pour trouver des armes n'exigent pas de mandat (mais une raison valide est requise).<br>
+					`Les fouilles d'armes n'exigent pas de mandat (mais une raison valide est requise).<br>
 						Vous POUVEZ fouiller les maisons sans raison.`,
-					`Les policiers corrompus ne peuvent pas contrecarrer activement l'application des lois. Par exemple, les policiers corrompus peuvent "ne pas notifier" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais il ne peut pas attaquer un autre policer.`,
+					`Les policiers corrompus ne peuvent pas contrecarrer activement les forces de l'ordre. Par exemple, les policiers corrompus peuvent "ne pas notifier" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais il ne peut pas attaquer un autre policer.`,
 					`Le maire n'est pas autorisé à faire des lois qui ne respectent pas les règles du serveur.`,
 					`La torture des prisonniers est régie par le maire ou le chef de la police.`,
 				]
@@ -103,8 +103,8 @@ module.exports = {
 			[
 				"Règles des commerçants & des médecins",
 				[
-					`Si un de vos clients vous agresse ou vous insulte, perturbe la paix dans votre échoppe ou est recherché par la police, vous pouvez vous réserver le droit de ne pas servir celui-ci.<br/>
-						Dans le cas contraire où le client n'a rien fait de mal, vous vous devez de le servir.`,
+					`Si un de vos clients vous agresse ou vous insulte, perturbe la paix dans votre échoppe ou est recherché par la police, vous pouvez vous réserver le droit de ne pas le servir.<br/>
+						Dans le cas contraire où le client est pacifique, vous vous devez de le servir.`,
 					`Les vendeurs d'armes, les médecins, les commerçants (Barman, etc) ne sont pas autorisés à utiliser leurs capacités à des fins criminelles ou de participer à des activités criminelles.`,
 					`Tous les hommes d'affaires devraient faire des profits sur leurs ventes.`,
 				]
@@ -131,7 +131,7 @@ module.exports = {
 					`Toute construction devra posséder une entrée, publiquement accessible avec un keypad ou un bouton et être crochettable.`,
 					`Toute entrée qui est faite par un mappeur devra être publiquement accessible avec un keypad ou un bouton et être crochettable.`,
 					`Les objets, cachés derrière/dessous/dessus un prop, devront être accessibles avec un keypad ou un bouton et être crochettables.`,
-					`Vous n'êtes pas autorisé à utiliser plus de deux couches de portes (les portes usuelles sont aussi prises en compte).<br>
+					`Vous n'êtes pas autorisé à utiliser plus de DEUX couches de portes (les portes usuelles sont aussi prises en compte).<br>
 						Si cette porte est de plus de deux props, il devra y avoir un keypad facilement visible près de la porte, qui ouvre toute l'entrée au même moment.`,
 					`Les boutons/keypads doivent être proches de l'objet qu'ils contrôlent et facilement visibles.`,
 					`Les entrées devront être capable de contenir deux joueurs et être en taille entière, ce qui signifie qu'elles doivent faire la taille d'un joueur qui se tient debout (64 unités), donc les tunnels qui demandent à être accroupis, ou les couloirs qui demandent des esquives/des sauts/des deviations ne sont pas autorisés.`,
@@ -142,14 +142,14 @@ module.exports = {
 					`Il est interdit d'utiliser un prop qui n'est transparent que d'un seul côté.`,
 					`Il est interdit de placer des structures gênantes sur des territoires qui ne vous appartiennent pas, sans le consentement du propriétaire.`,
 					`Vous pouvez déclarer un territoire neutre comme le votre et construire une maison dessus, si vous n'êtes pas dans un territoire public (route, parking).`,
-					`Il est interdit de faire apparaître des objets précieux et des bases dans des endroits, qui ne sont pas accessibles par les moyens de transports usuels (sans Parkour).<br>
+					`Il est interdit de faire apparaître des objets précieux (imprimantes, tourelles, etc) et des bases dans des endroits, qui ne sont pas accessibles par des moyens de transports usuels (sans Parkour).<br>
 						Exception: le toit d'un magasin, maximum 4 étages. Vous devez construire une échelle accessible publiquement ou un ascenseur jusqu'à celui-ci. Lorsqu'il n'y a pas de moyen pour y accéder, vous n'êtes pas autorisé à placer des objets précieux et des bases sur ce toit.`,
 					`Si un ascenseur est requis pour accéder à votre structure, contrôler celui-ci devra être possible sans crackage. Il est interdit de limiter l'accès de cet ascenseur. L'ascenseur devra marcher de manière stable. Ce qui veut dire:
 					<ul>
-						<li>Votre ascenseur devra être construit avec l'outil spécial DarkRP -> Elevator
+						<li>Votre ascenseur devra être construit avec l'outil spécial DarkRP -> Ascenseur.
 						<li>Il ne devra y avoir aucune porte ou mur à l'entrée ou à la sortie.
 					</ul>`,
-					`Il est interdit de faire des sols, des échelles, des ascenseurs et des plateformes disparaissants.`,
+					`Il est interdit de faire des sols, des échelles, des ascenseurs et des plateformes disparaissantes.`,
 					`Il est interdit de faire apparaître des prisons pour les joueurs/objets en utilisant le duplicateur sur des joueurs/objets, ou mettre des props sur des joueurs/objets avec le physgun.`,
 					`Il est interdit d'utiliser le wiremod pour rechercher des joueurs ou des objets précieux à l'extérieur de votre territoire.`,
 					`Vous n'êtes pas autorisé à construire durant un combat ou d'autres formes de confrontation.`,
