e35ada9d6087c5576533d99419aa49d834aa3563
Erwan Oliviero <oliviero@outlook.fr>
2017-07-12 23:15:31 +0200

    Update rules.js

--- french/website/rules.js
+++ french/website/rules.js
@@ -10,7 +10,7 @@ module.exports = {
 					"Il est interdit d'outrepasser le bannissement d'un compte.",
 					`Les joueurs doivent informer l'administration des problèmes majeurs, qui peuvent faire crash le serveur ou désavantager les autres joueurs.
 					<br>Il est punissable de posséder et de cacher de telles informations, ainsi que de les transmettre à d'autres joueurs.`,
-					"Votre nom ne doit pas contenir de lettres provenant de différentes langues."
+					"Votre nom ne doit pas contenir de lettres provenant de plusieures langues."
 				]
 			],
 			[
@@ -46,12 +46,12 @@ module.exports = {
 			[
 				"Section principale",
 				[
-					`Votre nom rôleplay doit être réaliste (et non Chat Miaou, L'Homme Crabe), il ne devra pas contenir de ponctuation et devra suivre les règles générales de capitalisation des mots.`,
+					`Votre nom rôleplay doit être réaliste (et non Chat Miaou, L'Homme Crabe), il ne devra pas contenir de ponctuation inutile et devra suivre les règles générales de capitalisation des mots (pas de noms tout en majuscules).`,
 					`Il est interdit de se suicider, de se déconnecter puis se reconnecter, ou bien même de changer de métier pour échapper à une situation RP, comme être poursuivi par la police, être braqué, être pris en otage, avoir faim ou autre.`,
 					`Votre but n'est pas de vaincre tout le monde, ne tuez pas chaque personne que vous voyez sur votre chemin. Tout meurtre devra être suivi d'une raison adéquate. (Random DeathMatch, RDM)`,
 					`Il est tout à fait raisonnable de tuer quelqu'un pour se venger. Toutefois, tuer une même personne plusieurs fois sera considéré à du RDM.`,
 					`Tout métier peut avoir une arme pour se défendre, sauf celles interdites par les lois.`,
-					`Les chefs de gangs sont "invisibles" - ils ressemblent à des citoyens normaux (ce qui signifie que vous ne pouvez pas fouiller un chef de gang, juste parce que vous voyez qu'il est chef).<br>
+					`Les gangsters et les chefs de gangs sont "invisibles" - ils ressemblent à des citoyens normaux (ce qui signifie que vous ne pouvez pas fouiller un gangster, juste parce que vous voyez qu'il est gangster).<br>
 						Toutes les autres informations affichées sur l'écran sont aussi vues par votre personnage.`,
 					`Ne changez pas de métier dans l'unique but d'aquérir des objets, des armes, des capacités ou n'importe quoi d'autre (JobAbuse).`,
 					`Ne faites pas quelque chose d'impossible qu'une personne réelle ne ferait pas, c'est-à-dire:
@@ -72,8 +72,8 @@ module.exports = {
 					`N'utilisez pas le chat Ville pour communiquer avec une seule personne en particulier.<br>
 						Pour discuter avec quelqu'un en privé utilisez le PM tab dans le chat.`,
 					`Il est interdit de commettre des actions rôleplay sexuellement explicites.`,
-					`Il est interdit d'envoyer un message en /advert, qui n'est pas une publicité RP.`,
-					`Évitez d'utiliser le chat vocal si votre voix est désagréable et qu'on vous le fait remarquer.`,
+					`Il est interdit d'envoyer une publicité en /advert, qui n'est pas une publicité RP.`,
+					`Évitez d'utiliser la radio vocale si vous êtes âgé de moins de 14 ans.`,
 					`N'utilisez pas le portable ainsi que le /advert pour appeler un admin.`,
 					`Ne menacez pas les autres joueurs de les bannir.`,
 				]
@@ -83,12 +83,12 @@ module.exports = {
 				[
 					`Les officiers de police ne sont pas autorisés à arrêter quelqu'un sans raison adéquate (Random Arrest).`,
 					`Utilisez le taser et les menottes uniquement contre les perturbateurs.`,
-					`Toutes les recherches, les couvre-feux et les fouilles d'armes (détecteur de métaux), doivent être suivis par une raison cohérente. Les raisons telles que "lol", "fouille", "ccccccc" sont invalides.`,
+					`Toutes les recherches ainsi que les couvre-feux, doivent être suivis par une raison cohérente. Les raisons telles que "lol", "fouille", "ccccccc" sont invalides.`,
 					//`Арест должен производиться только через кнопку "арестовать" в наручниках (а не ведением нарушителя в наручниках пешком до ПУ и удержания там).`,
 					`La police est autorisée à construire des barrières, des points de contrôle, des dos-d'ânes ainsi qu'à bloquer des rues ou des bâtiments. Toutes les barrières qui bloquent quelque chose doivent avoir un entrée crackable.`,
 					`Les fouilles d'armes n'exigent pas de mandat (mais une raison valide est requise).<br>
 						Vous POUVEZ fouiller les maisons sans raison.`,
-					`Les policiers corrompus ne peuvent pas contrecarrer activement les forces de l'ordre. Par exemple, les policiers corrompus peuvent "ne pas notifier" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais il ne peut pas attaquer un autre policer.`,
+					`Les policiers corrompus ne peuvent pas agir contre les forces de l'ordre. Par exemple, les policiers corrompus peuvent "ne pas prendre en compte" certains crimes ou "accidentellement" oublier de fermer la porte de la prison; mais ils ne peuvent pas attaquer un autre policer.`,
 					`Le maire n'est pas autorisé à faire des lois qui ne respectent pas les règles du serveur.`,
 					`La torture des prisonniers est régie par le maire ou le chef de la police.`,
 				]
@@ -96,7 +96,7 @@ module.exports = {
 			[
 				"Règles des gangs",
 				[
-					`Les chefs de gangs ne sont pas dans la même équipe. Ils peuvent être l'un contre l'autre.`,
+					`Les gangsters ne sont pas dans la même équipe. Ils peuvent être l'un contre l'autre.`,
 					`Les gangs seront comptés valides, uniquement s'ils ont été créés à partir du système de gang.`,
 				]
 			],
@@ -105,7 +105,7 @@ module.exports = {
 				[
 					`Si un de vos clients vous agresse ou vous insulte, perturbe la paix dans votre échoppe ou est recherché par la police, vous pouvez vous réserver le droit de ne pas le servir.<br/>
 						Dans le cas contraire où le client est pacifique, vous vous devez de le servir.`,
-					`Les vendeurs d'armes, les médecins, les commerçants (Barman, etc) ne sont pas autorisés à utiliser leurs capacités à des fins criminelles ou de participer à des activités criminelles.`,
+					`Les vendeurs d'armes, les commerçants, les barmans, les pompiers, les médecins et les policiers ne sont pas autorisés à utiliser leurs capacités à des fins criminelles ou de participer à des activités criminelles.`,
 					`Tous les hommes d'affaires devraient faire des profits sur leurs ventes.`,
 				]
 			],
@@ -120,7 +120,7 @@ module.exports = {
 			[
 				"Règles de construction",
 				[
-					`Il est interdit de faire apparaître temporairement un objet pour atteindre une position inaccessible (PropClimb).`,
+					`Il est interdit de faire temporairement apparaître un prop ou un objet pour atteindre une position inaccessible (PropClimb).`,
 					`Il est interdit de tuer avec un objet (PropKill).`,
 					`Il est interdit de voler avec la force du physgun (PropSurf).`,
 					`Il est interdit de faire apparaître plein d'objets en un endroit et/ou dans une courte période de temps (PropSpam).`,
@@ -130,7 +130,7 @@ module.exports = {
 						Si l'entrée est suivie d'un couloir et que les portes vous appartiennent, vous êtes autorisé à la bloquer.`,
 					`Toute construction devra posséder une entrée, publiquement accessible avec un keypad ou un bouton et être crochettable.`,
 					`Toute entrée qui est faite par un mappeur devra être publiquement accessible avec un keypad ou un bouton et être crochettable.`,
-					`Les objets, cachés derrière/dessous/dessus un prop, devront être accessibles avec un keypad ou un bouton et être crochettables.`,
+					`Les objets, cachés derrière/dessous/dans un prop, devront être accessibles avec un keypad ou un bouton et être crochettables.`,
 					`Vous n'êtes pas autorisé à utiliser plus de DEUX couches de portes (les portes usuelles sont aussi prises en compte).<br>
 						Si cette porte est de plus de deux props, il devra y avoir un keypad facilement visible près de la porte, qui ouvre toute l'entrée au même moment.`,
 					`Les boutons/keypads doivent être proches de l'objet qu'ils contrôlent et facilement visibles.`,
@@ -141,7 +141,7 @@ module.exports = {
 					`Il est interdit d'utiliser un prop qui ressemble à un mur, mais qui finalement agit comme un mur sans collision et tirer à travers et vice versa.`,
 					`Il est interdit d'utiliser un prop qui n'est transparent que d'un seul côté.`,
 					`Il est interdit de placer des structures gênantes sur des territoires qui ne vous appartiennent pas, sans le consentement du propriétaire.`,
-					`Vous pouvez déclarer un territoire neutre comme le votre et construire une maison dessus, si vous n'êtes pas dans un territoire public (route, parking).`,
+					`Vous pouvez déclarer un territoire neutre comme le votre et construire dessus, si vous n'êtes pas dans un territoire public (route, parking, égoûts).`,
 					`Il est interdit de faire apparaître des objets précieux (imprimantes, tourelles, etc) et des bases dans des endroits, qui ne sont pas accessibles par des moyens de transports usuels (sans Parkour).<br>
 						Exception: le toit d'un magasin, maximum 4 étages. Vous devez construire une échelle accessible publiquement ou un ascenseur jusqu'à celui-ci. Lorsqu'il n'y a pas de moyen pour y accéder, vous n'êtes pas autorisé à placer des objets précieux et des bases sur ce toit.`,
 					`Si un ascenseur est requis pour accéder à votre structure, contrôler celui-ci devra être possible sans crackage. Il est interdit de limiter l'accès de cet ascenseur. L'ascenseur devra marcher de manière stable. Ce qui veut dire:
