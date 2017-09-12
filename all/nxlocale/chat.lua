0897413d92c62af45916027463751e46460739d8
nx <noiwex1911@gmail.com>
2017-09-12 12:43:46 +0300

    nxlocale

---
+++ all/nxlocale/chat.lua
@@ -0,0 +1,157 @@
+return {
+	en = {
+		options = {
+			blur = "Blur",
+			noprivacy = "Show PM in game chat",
+			timestamps = "Timestamps in game chat",
+			fontsize = "Font Size",
+			default = "default",
+		},
+
+		pm = {
+			close = "Close",
+			block = "Block PMs",
+			unblock = "Unblock PMs",
+			open_profile = "Open Profile",
+			copy_sid = "Copy SteamID",
+			pm = "PM",
+			sent_to = "To",
+		},
+
+		context = {
+			player = {
+				pm = "PM",
+				mon = "Monitoring filter",
+				goto = "Goto",
+				goto_silent = "Goto (silent)",
+				open_profile = "Open profile",
+				copy_profile = "Copy profile link",
+				copy_sid = "Copy SteamID",
+			},
+			link = {
+				copy = "Copy link",
+			},
+			text = {
+				copy = "Copy",
+				all = "Select All",
+				google = "Search in Google",
+			},
+		},
+
+		disconnected_player = "Disconnected Player",
+
+		channels_hint = "Psst... Try pressing Tab and Ctrl-Tab!",
+		prefixes_hint = {
+			"We noticed that you're using chat prefixes. You don't need to. Try pressing",
+			"to switch channels.",
+		},
+
+		gps_button = "Embed current location",
+		sent_gps_button = "LOCATION",
+		gps_marker = "Location from chat",
+	},
+
+	ru = {
+		options = {
+			blur = "Размытие",
+			noprivacy = "Показывать ЛС в общем чате",
+			timestamps = "Время у каждого сообщения",
+			fontsize = "Размер шрифта",
+			default = "обычный",
+		},
+
+		pm = {
+			close = "Закрыть",
+			block = "Заблокировать ЛС",
+			unblock = "Разблокировать ЛС",
+			open_profile = "Открыть профиль",
+			copy_sid = "Скопировать SteamID",
+			pm = "ЛС",
+			sent_to = "Исходящее",
+		},
+
+		context = {
+			player = {
+				pm = "ЛС",
+				mon = "Фильтровать в мониторинге",
+				goto = "Подойти",
+				goto_silent = "Подойти (тихо)",
+				open_profile = "Открыть профиль",
+				copy_profile = "Скопировать ссылку на профиль",
+				copy_sid = "Скопировать SteamID",
+			},
+			link = {
+				copy = "Скопировать ссылку",
+			},
+			text = {
+				copy = "Скопировать",
+				all = "Выделить всё",
+				google = "Поиск в Google",
+			},
+		},
+
+		disconnected_player = "Отключившийся игрок",
+
+		channels_hint = "Псст... Попробуйте нажимать Tab и Ctrl-Tab!",
+		prefixes_hint = {
+			"Мы заметили, что вы используете префиксы в сообщениях. Теперь вам не нужно это делать. Попробуйте нажать",
+			"чтобы переключить канал.",
+		},
+
+		gps_button = "Вставить текущую локацию",
+		sent_gps_button = "ЛОКАЦИЯ",
+		gps_marker = "Локация из чата",
+	},
+
+	fr = {
+		options = {
+			blur = "Blur",
+			noprivacy = "Show PM in game chat",
+			timestamps = "Timestamps in game chat",
+			fontsize = "Font Size",
+			default = "default",
+		},
+
+		pm = {
+			close = "Close",
+			block = "Block PMs",
+			unblock = "Unblock PMs",
+			open_profile = "Open Profile",
+			copy_sid = "Copy SteamID",
+			pm = "PM",
+			sent_to = "To",
+		},
+
+		context = {
+			player = {
+				pm = "PM",
+				mon = "Monitoring filter",
+				goto = "Goto",
+				goto_silent = "Goto (silent)",
+				open_profile = "Open profile",
+				copy_profile = "Copy profile link",
+				copy_sid = "Copy SteamID",
+			},
+			link = {
+				copy = "Copy link",
+			},
+			text = {
+				copy = "Copy",
+				all = "Select All",
+				google = "Search in Google",
+			},
+		},
+
+		disconnected_player = "Disconnected Player",
+
+		channels_hint = "Psst... Try pressing Tab and Ctrl-Tab!",
+		prefixes_hint = {
+			"We noticed that you're using chat prefixes. You don't need to. Try pressing",
+			"to switch channels.",
+		},
+
+		gps_button = "Embed current location",
+		sent_gps_button = "LOCATION",
+		gps_marker = "Location from chat",
+	},
+}
