99bba0d6e2dc16d3eb63d3040e2e89dc6dc0a5de
nx <noiwex1911@gmail.com>
2017-10-23 20:55:42 +0300

    yelo

---
+++ all/nxlocale/crashes.lua
@@ -0,0 +1,25 @@
+return {
+	en = {
+		instructions = [[<color=red><font=DermaLarge>Server crashed, now what?</font></color>
+	- Keep patience and save your contraptions often next time!
+	- If crash happened just after you did something, then report it to the forums
+	(this might be useful to us, so we can fix crashes with your help)
+	- Wait until server restart (it takes from 30 seconds to 3 minutes)
+	- Continue playing!
+
+Disable auto-reconnect: Press F1 (press twice to reconnect now)
+]],
+	},
+
+	ru = {
+		instructions = [[<color=red><font=DermaLarge>Сервер упал, что теперь?</font></color>
+	- Сохраняйте спокойствие и в следующий раз чаще сохраняйте постройки!
+	- Если креш произошел сразу после того, как вы что-то сделали, то напишите об этом на форум
+	(это может быть полезным для нас, так как мы можем устранить эти креши с вашей помощью)
+	- Подождите пока сервер перезагружается (Это занимает от 30 секунд до трех минут)
+	- Продолжайте играть!
+
+Отключить автореконнект: Нажмите F1 (нажмите дважды чтобы переподключиться сейчас)
+]],
+	},
+}
