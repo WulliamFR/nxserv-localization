c3cca10c5260f6e2badb0bf4421547a7892f885b
nx <noiwex1911@gmail.com>
2017-12-27 19:32:33 +0300

    added tos

---
+++ french/website/tos.js
@@ -0,0 +1,44 @@
+const terms = [
+	`It is absolutely free to play on our servers, if you own a Steam account with Garry's Mod license.`,
+
+	`Users can make and publish their content with use of recordings from our servers, and also monetize it. However, we reserve the right to ask you stop doing so.`,
+
+	`You agree that you will not use any software or hardware to gain unfair advantage during game process on our servers. Any ban for such activity can not be disputed.`,
+
+	`You agree not to use additional accounts, proxies to bypass bans on our servers. All additional accounts will be blocked.`,
+
+	`You agree not to disrupt server operation by any means.`,
+
+	`You agree to tell the administration about all important problems, which may disrupt server operation or gain unfair advantage.
+	It is punishable to own and not disclose such information, as well as transmission of it to other persons.`,
+
+	`You agree not to exchange in-game currency and items to real currency and its equialents, using any forms of interaction besides in-game NxServ Shop and Shop at https://nxserv.eu/.`,
+
+	`You agree not to use resources of NxServ to spread and accquire information, that is prohibited by laws and institutions of Russian Federation, or information that violates privacy of any persons.`,
+
+	`You acknowledge and agree that all payments and other expenses related to paid services are paid in advance and <b>they're not eligible for refund in full or partial amounts</b>. <u>You are fully responsible for all expenses, related to your accounts, including <b>ANY</b> unauthorized payments or actions.</u>`,
+
+	`All transactions and purchases are processed automatically in any time of day, with exception for technical downtimes or failures in operations of payment agents.`,
+
+	`You acknowledge and agree that purchase of item on NxServ servers means us giving us access to item or service on NxServ resources during item/service lifetime period. Purchase of virtual item/service does not imply transfer of item/service ownership to purchaser. In the event of account block or ToS termination access to items/services will be blocked or lost, compensation will not be done in full or partial manner.`,
+
+	`You agree not to conduct any suspicious or fraudlent activities related to payment systems. NxServ monitors all operations, and may block your account and contact law enforcement services in the event of problems.`,
+
+	`You agree not to violate NxServ copyright on any content or code.`,
+
+	`You agree to follow these terms and game rules on servers.`,
+
+	`NxServ reserves the right to modify, add or delete virtual items/services at any time.`,
+
+	`NxServ reserves the right to ban all user's accounts for violation of any part of this agreement, or any part of game rules, or in the event of ToS termination.`,
+
+	`NxServ reserves the right to scan temporary and permanent memory, devices and network of users during game in order to detect signatures of forbidden software or devices.`,
+
+	`NxServ doesn't give any garanties, does not bear responsibility for any damage or other obligations, occured during use of software and/or any other interaction with objects of property of NxServ.`,
+
+	`NxServ reserves the right to terminate your agreement with us at any time, for any reason without explanations, notifications or warnings.`,
+
+	`NxServ reserves the right to modify this agreement at any time without notification.`,
+]
+
+export default terms;
